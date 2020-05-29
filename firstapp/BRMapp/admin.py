fd930627a2ac3d44994f23a9813b1c2943dfd872
Saket Suman <sumansaket7@gmail.com>
2020-05-30 02:08:21 +0530

    project submitted

---
+++ firstapp/BRMapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from BRMapp.models import BRMuser
+# Register your models here.
+
+
+admin.site.register(BRMuser)
---
+++ firstapp/BRMapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class NewBookForm(forms.Form):  #this Form class  will be child class of forms class under forms module
+    title=forms.CharField(label='Title',max_length=100)
+    price=forms.FloatField(label='Price')
+    author=forms.CharField(label='Author')
+    publisher=forms.CharField(label='publisher')
+
+
+class SearchForm(forms.Form):
+    title=forms.CharField(label='Title',max_length=100)
+    
